54dcc2d78787a75e49512fbcf34498b1691b36f2
Ruman Maharjan <rumanmaharjan770@gmail.com>
2023-01-09 15:53:51 +0545

    async added

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app = express();
-
 app.use(express.json());
-
+(async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+       
+      });
+  
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+      process.exit(1);
+    }
+  })();
 app.get("/", (req, res) => {
   //   res.send("API is running, fine, have a good day...");
   res.send({ message: "Hello , its your first get request!", 
