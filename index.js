e6f37698834b82f3e919bedef6c63f10958168ed
Ruman Maharjan <rumanmaharjan770@gmail.com>
2023-01-09 15:29:52 +0545

    add a post request with json parser enable

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello Mern");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  //   res.send("API is running, fine, have a good day...");
+  res.send({ message: "Hello , its your first get request!", 
+  status: "fine", 
+});
+});
+
+app.post("/test", (req, res) => {
+  res.send(req.body);
+});
+
+app.listen(3005, console.log("Express app serverd at 3005"));
+// console.log("Hello Mern!");
