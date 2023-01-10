80a2adc51d72218d902333766f58a9ed863bfe92
Ruman Maharjan <rumanmaharjan770@gmail.com>
2023-01-10 15:51:05 +0545

    added request handlers for create, list, get user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import User from "./models/users.js";
 
 dotenv.config();
 const app = express();
@@ -25,10 +26,83 @@ app.get("/", (req, res) => {
   status: "fine", 
 });
 });
-
+app.post("/api/users", (req, res) => {
+    const{ fullName,email,password, ...rest } = req.body;
+    // console.log("\n>>>>>>\n", req, "\n>>>>>>>\n");
+  User.create({
+   fullName,
+   email,
+   password,
+  }).then(response => console.log(response))
+    .catch(err => console.log(err));
+    res.send({message:"User Created!"});
+});
+app.get("/api/users", (req, res) => {
+    User.find({
+}).then((response) => {
+    res.send({
+        message: "user listed",
+        users: response,
+    });
+    console.log(response);
+  })
+  .catch((err) => {
+      res.send({
+          message: "Errors getting users",
+          users: JSON.stringify(err),
+      });
+      console.log(err)});
+  });
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+    User.findById({})
+    .then((response) => {
+    res.send({
+        message: "user retrieved",
+        user: response,
+    });
+    console.log(response);
+})
+.catch((err) => {
+    res.send({
+        message: "Errors getting users",
+        users: JSON.stringify(err),
+    });
+    console.log(err)});
+});
 app.post("/test", (req, res) => {
+    console.log("\n>>>>>>\n", req, "\n>>>>>>>\n");
   res.send(req.body);
 });
 
-app.listen(3005, console.log("Express app serverd at 3005"));
+app.put("/api/users", (req, res) => {
+    const{ fullName,email,password, ...rest } = req.body;
+    // console.log("\n>>>>>>\n", req, "\n>>>>>>>\n");
+  User.findOneAndUpdate( {email},{
+   fullName,
+   email,
+   password,
+  }).then(response => console.log(response))
+    .catch(err => console.log(err));
+    res.send({message:"User Created!"});
+});
+
+app.listen(process.env.PORT, console.log(`Express app serverd at 3005:${process.env.PORT}`));
 // console.log("Hello Mern!");
+// app.post("api/auth/login",(req,res) =>{
+//     const{email, password} = req.body;
+//     User.findOne({email}).then(response => {
+//        if(response.password = password){
+//         res.send({message:"User authenticated!"});
+
+//        }
+//        else
+//        { res.send({message:"Wrong password"});}
+      
+//     }).catch(err => {
+//         res.send({
+
+//         })
+//     }
+//         )
+// })
