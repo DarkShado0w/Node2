bbe17ff7bcb108e45e8ed37ef17bbbc717170f72
Ruman Maharjan <rumanmaharjan770@gmail.com>
2023-01-17 22:59:55 +0545

    import schemas

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -6,10 +6,12 @@ import {
   listProduct,
   updateProduct,
 } from "../controllers/productController.js";
+import validateData from "../middlewares/validation.js";
+import productSchema from "../schemas/products.js";
 
 const productsRouter = Router();
 
-productsRouter.route("/").get(listProduct).post(createProduct);
+productsRouter.route("/").get(listProduct).post(validateData(productSchema),createProduct);
 
 productsRouter
   .route("/:id")
