0d1ce9dc2ce7f787f2757f5b6f126fd9bf80e7b9
Ruman Maharjan <rumanmaharjan770@gmail.com>
2023-01-17 23:00:24 +0545

    import userschema

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { createUser, deleteUser, findUser, listUser, updateUser } from "../controllers/usersController.js";
+import validateData from "../middlewares/validation.js";
+import userSchema from "../schemas/users.js";
 const usersRouter = Router();
-usersRouter.route("/").get(listUser).post(createUser);
+usersRouter.route("/").get(listUser).post(validateData(userSchema), createUser);
 usersRouter.route("/:id").get(findUser).put(updateUser).delete(deleteUser);
 export default usersRouter;
